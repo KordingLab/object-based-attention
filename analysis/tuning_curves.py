a5879daa4829353b80e7a16ad33e5df68ec112aa
jordanlei <thunderflash.lei@gmail.com>
2021-05-24 11:11:59 -0500

    new tuning curve plots

--- analysis/tuning_curves.py
+++ analysis/tuning_curves.py
@@ -155,7 +155,6 @@ def plot_curves(attended_activations, not_attended_activations, degrange):
             c = 3
 
         for i in range(act.shape[0]): 
-            plt.figure(figsize = (3, 3))
             plt.plot(degrange, act[i][r][c], color = (14/255, 77/255, 179/255, 1))
             plt.plot(degrange, nonact[i][r][c], color = (212/255, 78/255, 78/255, 1))
             plt.xlabel("Degrees")
